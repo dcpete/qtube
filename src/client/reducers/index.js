c8fcc1eea38b151a750eeb03784788e6cec48186
Donny Peterson <peterson.donaldc@gmail.com>
2018-05-26 01:51:10 -0400

    Added user reducer

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 
+import UserReducer from './user_reducer';
+
 const rootReducer = combineReducers({
+  user: UserReducer
 });
 
 export default rootReducer;
