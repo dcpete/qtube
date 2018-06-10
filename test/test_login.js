1c663482ed6bb5e37d76836005635d6cdda31ee4
Donny Peterson <peterson.donaldc@gmail.com>
2018-06-10 16:56:28 -0400

    Added tests for login with username, changed parameters to use objects

--- test/test_login.js
+++ test/test_login.js
@@ -15,36 +15,93 @@ describe("AUTH - LOGIN (not authenticated)", () => {
       });
   });
 
-  it("should allow a user to login", () => {
-    return fn.logInTestUser(testuser.email, testuser.password)
+  it("should allow a user to login with their email address", () => {
+    return fn.logInTestUser({
+      email: testuser.email,
+      password: testuser.password
+    })
       .then(res => {
         expect(res.status).to.be.equal(200);
         expect(res.body.username).to.be.equal(testuser.username);
       })
   });
 
+  it("should allow a user to login with their username", () => {
+    return fn.logInTestUser({
+      username: testuser.username,
+      password: testuser.password
+    })
+      .then(res => {
+        expect(res.status).to.be.equal(200);
+        expect(res.body.username).to.be.equal(testuser.username);
+      })
+  });
+
+  it("should return 400 if no email or username are supplied", () => {
+    return fn.logInTestUser({
+      password: testuser.password
+    })
+      .then(res => {
+        expect(res.status).to.be.equal(400);
+      })
+  });
+  
+  it("should return 400 if both email and username are supplied", () => {
+    return fn.logInTestUser({
+      username: testuser.username,
+      email: testuser.email,
+      password: testuser.password
+    })
+      .then(res => {
+        expect(res.status).to.be.equal(400);
+      })
+  });
+
   it("should return 400 for an email in a bad format", () => {
-    return fn.logInTestUser(badcreds.notemailformat, testuser.password)
+    return fn.logInTestUser({
+      email: badcreds.notemailformat,
+      password: testuser.password
+    })
       .then(res => {
         expect(res.status).to.be.equal(400);
       })
   });
 
   it("should return 400 for a password that is too short", () => {
-    return fn.logInTestUser(testuser.email, badcreds.shortPassword)
+    return fn.logInTestUser({
+      email: testuser.email,
+      password: badcreds.shortPassword
+    })
       .then(res => {
         expect(res.status).to.be.equal(400);
       });
   });
+
+  it("should return 401 for an unknown username", () => {
+    return fn.logInTestUser({
+      username: badcreds.unknownUsername,
+      password: testuser.password
+    })
+      .then(res => {
+        expect(res.status).to.be.equal(401);
+      })
+  });
+
   it("should return 401 for an unknown email", () => {
-    return fn.logInTestUser(badcreds.unknownEmail, testuser.password)
+    return fn.logInTestUser({
+      email: badcreds.unknownEmail,
+      password: testuser.password
+    })
       .then(res => {
         expect(res.status).to.be.equal(401);
       })
   });
 
   it("should return 401 for a bad password", () => {
-    return fn.logInTestUser(testuser.email, badcreds.incorrectPassword)
+    return fn.logInTestUser({
+      email: testuser.email,
+      password: badcreds.incorrectPassword
+    })
       .then(res => {
         expect(res.status).to.be.equal(401);
       });
